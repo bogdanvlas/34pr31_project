b35b6eac61dcaf8133c55bd23fd641624e63c316
Bogdan <vlasenko_b@itstep.academy>
2021-08-20 18:50:09 +0300

    20.08.2021-1

--- src/main/java/com/itstep/my_secured_app/controller/PageController.java
+++ src/main/java/com/itstep/my_secured_app/controller/PageController.java
@@ -2,13 +2,19 @@ package com.itstep.my_secured_app.controller;
 
 import com.itstep.my_secured_app.model.User;
 import com.itstep.my_secured_app.repository.UserRepository;
+import com.itstep.my_secured_app.service.MailService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Map;
+import java.util.Random;
 
 @Controller
 @AllArgsConstructor
@@ -16,6 +22,8 @@ public class PageController {
     private UserRepository userRepository;
 
     private PasswordEncoder passwordEncoder;
+
+    private MailService mailService;
 
     @GetMapping
     public String index() {
@@ -57,5 +65,37 @@ public class PageController {
         //записать пользователя в БД
         userRepository.save(user);
         return "redirect:/";
+    }
+
+    @PostMapping("/forgot-psw")
+    public void forgotPassword(@RequestBody Map<String, String> body) {
+        String username = body.get("username");
+        //найти пользователя
+        User user = userRepository.findByUsername(username);
+        //сгенерировать новый случайный пароль
+        String newPassword = generatePassword(8);
+        //отправить новый пароль на почту пользователя
+        String text = "Dear " + username + ", this is your new password: " + newPassword;
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setText(text);
+        mail.setTo(user.getEmail());
+        mail.setFrom("34pr31");
+        mail.setSubject("New password");
+        mailService.sendEmail(mail);
+        //закодировать и записать пароль пользователю
+        newPassword = passwordEncoder.encode(newPassword);
+        user.setPassword(newPassword);
+        userRepository.save(user);
+    }
+
+    public String generatePassword(int len) {
+        String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy";
+        Random rnd = new Random();
+
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+        return sb.toString();
     }
 }
